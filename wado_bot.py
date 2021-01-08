66979c52681f20155e6071d0da9531b6b07bbf4d
miladiashe <dkdud0122@naver.com>
2021-01-09 02:32:55 +0900

    Create wado_bot.py

---
+++ wado_bot.py
@@ -0,0 +1,10 @@
+endFlag = 0
+print("와들디와 연결되었습니다.")
+print("대화를 끝내고 싶으면 '잘 가!'라고 입력해 주세요!")
+while(endFlag == 0):
+    yourText = input("당신:")
+    if(yourText == "잘 가!"):
+        endflag = 1
+        break
+    print("와들디: 와냐!")
+print("와들디: 와냐와냐~")
